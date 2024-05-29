044d61d4c757e787f6c69d940d6e2ca704a48607
Laura Nuñez <njm0031533@est.univalle.edu>
2024-05-29 14:04:26 -0400

    Referencia agregada

--- PDI_Mosaico/PDI_Mosaico/MainWindow.xaml.cs
+++ PDI_Mosaico/PDI_Mosaico/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing.Imaging;
+using AForge.Imaging.Filters;
 
 namespace PDI_Mosaico
 {
